4292026f51166a811f8a73ab2d04dccc93a86911
Kirill Sychevnik <kirill.sychevnik@intexsoft.by>
2022-01-31 18:16:29 +0300

    fixed bugs

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,21 @@ const burgerMenuFunction = () => {
   const headerContainer = document.querySelector('.header');
   const burgerBtn = headerContainer.querySelector('.menu-btn');
   const navMenu = headerContainer.querySelector('.nav-menu');
+  const navLink = headerContainer.querySelectorAll('.nav-menu__link')
 
   function handleBurgerToggle() {
-    this.classList.toggle('is-active');
+    burgerBtn.classList.toggle('is-active');
     navMenu.classList.toggle('nav-menu_active');
   }
 
+
+  navLink.forEach(item => {
+    if (window.outerWidth < 581) {
+      item.addEventListener('click', handleBurgerToggle)
+    }
+  })
+
+  
   burgerBtn.addEventListener('click', handleBurgerToggle);
 };
 
